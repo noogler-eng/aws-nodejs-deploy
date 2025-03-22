1c9c37ce9cd55fbd49ef222d6ad9275aea96c837
noogler-eng <sharadpoddar10102003@gmail.com>
2025-03-22 14:15:21 +0530

    script and server

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { rateLimit } from "express-rate-limit";
+import { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+});
+
+app.use(cors());
+app.use(limiter);
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    msg: "server is working",
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log("server listening at: ", PORT);
+});
